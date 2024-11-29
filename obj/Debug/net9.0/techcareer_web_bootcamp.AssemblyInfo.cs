a7707a525293c2b19aa3b44549832e80026abb29
Morteza <moradimurtaza84@gmail.com>
2024-11-29 03:02:00 +0300

    project ready for deployment

--- obj/Debug/net9.0/techcareer_web_bootcamp.AssemblyInfo.cs
+++ obj/Debug/net9.0/techcareer_web_bootcamp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("techcareer_web_bootcamp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c2879eff86656662549f3100d822fdfcdd8a9cbc")]
 [assembly: System.Reflection.AssemblyProductAttribute("techcareer_web_bootcamp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("techcareer_web_bootcamp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
